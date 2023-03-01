e951af83d90b8febbfd43cfd925c5998b7bac558
Roman.K <akupaka@gmail.com>
2023-03-01 10:22:55 +0200

    Initial Commit

---
+++ Exchange Rates/Exchange_RatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exchange_RatesApp.swift
+//  Exchange Rates
+//
+//  Created by Roman K on 01.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Exchange_RatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
